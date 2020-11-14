4edaf2c9b8b8b8cdcb8abb79c1a808075a6f259a
GreenScripter <29268195+GreenScripter@users.noreply.github.com>
2020-11-14 15:18:33 -0800

    Added the option to change the speed of boat fly. resolves #10

--- src/main/java/net/wurstclient/hacks/BoatFlyHack.java
+++ src/main/java/net/wurstclient/hacks/BoatFlyHack.java
@@ -13,14 +13,22 @@ import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.settings.SliderSetting.ValueDisplay;
+import net.wurstclient.util.RotationUtils;
 
 @SearchTags({"BoatFlight", "boat fly", "boat flight"})
 public final class BoatFlyHack extends Hack implements UpdateListener
 {
+	public final SliderSetting speed =
+		new SliderSetting("Speed", 1, 1, 20, 0.05, ValueDisplay.DECIMAL);
+	
 	public BoatFlyHack()
 	{
 		super("BoatFly", "Allows you to fly with boats");
 		setCategory(Category.MOVEMENT);
+		addSetting(speed);
+
 	}
 	
 	@Override
@@ -45,7 +53,12 @@ public final class BoatFlyHack extends Hack implements UpdateListener
 		// fly
 		Entity vehicle = MC.player.getVehicle();
 		Vec3d velocity = vehicle.getVelocity();
-		double motionY = MC.options.keyJump.isPressed() ? 0.3 : 0;
+		double motionY = MC.options.keyJump.isPressed() ? speed.getValueF() : 0;
+		if (speed.getValueF() != 1 && velocity.length() >= 0.1 && MC.options.keyForward.isPressed()) {
+			velocity = RotationUtils.getMoveVec(vehicle.yaw);
+			velocity = new Vec3d(velocity.x, 0, velocity.z).normalize().multiply(speed.getValue());
+		}
 		vehicle.setVelocity(new Vec3d(velocity.x, motionY, velocity.z));
+		
 	}
 }
