30fdd9ecc25db69333daf808d1a45daf2bdca15d
GreenScripter <29268195+GreenScripter@users.noreply.github.com>
2020-11-17 19:55:56 -0800

    Added Anti Levitation hack

---
+++ src/main/java/net/wurstclient/hacks/AntiLevitationHack.java
@@ -0,0 +1,14 @@
+package net.wurstclient.hacks;
+
+import net.wurstclient.Category;
+import net.wurstclient.hack.Hack;
+
+public final class AntiLevitationHack extends Hack
+{
+	public AntiLevitationHack()
+	{
+		super("AntiLevitation", "Prevent you from levitating with the levitation effect.");
+		setCategory(Category.MOVEMENT);
+	}
+	
+}
---
+++ src/main/java/net/wurstclient/mixin/LivingEntityMixin.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 - 2020 | Alexander01998 | All rights reserved. This source code is subject to
+ * the terms of the GNU General Public License, version 3. If a copy of the GPL was not distributed
+ * with this file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
+import net.wurstclient.WurstClient;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+	
+	@Inject(at = {@At("HEAD")},
+			method = {"hasStatusEffect(Lnet/minecraft/entity/effect/StatusEffect;)Z"},
+			cancellable = true)
+	public void hasStatusEffect(StatusEffect effect, CallbackInfoReturnable<Boolean> cir) {
+		if (WurstClient.MC.player == (Object)this && WurstClient.INSTANCE.getHax().antiLevitationHack.isEnabled() && effect.equals(StatusEffects.LEVITATION)) {
+			cir.setReturnValue(false);
+		}
+	}
+	
+}
