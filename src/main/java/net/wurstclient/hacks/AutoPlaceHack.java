708deec5da9650855a758c0cd2a84336c5b00510
GreenScripter <29268195+GreenScripter@users.noreply.github.com>
2020-11-29 17:31:54 -0800

    Added AutoPlace hack

---
+++ src/main/java/net/wurstclient/hacks/AutoPlaceHack.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2014-2020 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.hacks;
+
+import net.wurstclient.Category;
+import net.wurstclient.SearchTags;
+import net.wurstclient.events.UpdateListener;
+import net.wurstclient.hack.Hack;
+import net.wurstclient.mixinterface.IKeyBinding;
+
+@SearchTags({"auto use", "auto place"})
+public final class AutoPlaceHack extends Hack implements UpdateListener
+{
+	public AutoPlaceHack()
+	{
+		super("AutoPlace", "Automatically holds the use key.");
+		setCategory(Category.BLOCKS);
+	}
+	
+	@Override
+	public void onEnable()
+	{
+		EVENTS.add(UpdateListener.class, this);
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		EVENTS.remove(UpdateListener.class, this);
+		MC.options.keyUse.setPressed(((IKeyBinding) MC.options.keyUse).isActallyPressed());
+	}
+	
+	@Override
+	public void onUpdate()
+	{
+		MC.options.keyUse.setPressed(true);
+
+	}
+}
