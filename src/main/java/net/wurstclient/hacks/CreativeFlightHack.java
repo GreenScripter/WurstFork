93b1f565bfc7a8bd4d97fd2b5eb390e384c5ae9b
GreenScripter <29268195+GreenScripter@users.noreply.github.com>
2021-09-06 01:19:11 -0700

    Added Anti flight kick to creative fly hack.

--- src/main/java/net/wurstclient/hacks/CreativeFlightHack.java
+++ src/main/java/net/wurstclient/hacks/CreativeFlightHack.java
@@ -13,10 +13,20 @@ import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
 import net.wurstclient.events.UpdateListener;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.CheckboxSetting;
+import net.wurstclient.settings.SliderSetting;
+import net.wurstclient.settings.SliderSetting.ValueDisplay;
 
 @SearchTags({"creative flight", "CreativeFly", "creative fly"})
 public final class CreativeFlightHack extends Hack implements UpdateListener
 {
+	public final CheckboxSetting anti_flykick = new CheckboxSetting(
+		"Anti Flykick", "Bypass the Fly check on Vanilla Servers.", false);
+	public final SliderSetting decentRate = new SliderSetting("Decent Delay",
+		"Controls the rate of decent while flying.\nHelps prevent laggy servers from kicking you.",
+		40, 1, 40, 1, ValueDisplay.INTEGER);
+	private int fly_ticks = 0;
+	
 	public CreativeFlightHack()
 	{
 		super("CreativeFlight",
@@ -25,6 +35,8 @@ public final class CreativeFlightHack extends Hack implements UpdateListener
 				+ " You will take fall damage if you don't use NoFall.");
 		
 		setCategory(Category.MOVEMENT);
+		addSetting(anti_flykick);
+		addSetting(decentRate);
 	}
 	
 	@Override
@@ -53,5 +65,16 @@ public final class CreativeFlightHack extends Hack implements UpdateListener
 	public void onUpdate()
 	{
 		MC.player.getAbilities().allowFlying = true;
+		if(anti_flykick.isChecked())
+		{
+			fly_ticks--;
+			if(fly_ticks <= 0)
+			{
+				fly_ticks = decentRate.getValueI();
+				if(MC.player.getVelocity().y >= 0)
+					MC.player.setVelocity(MC.player.getVelocity().subtract(0,
+						MC.player.getVelocity().y + 0.05, 0));
+			}
+		}
 	}
 }
